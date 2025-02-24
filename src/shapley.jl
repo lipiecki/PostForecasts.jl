652b297770ad472bec4e31d8d66fb83e16d91f3a
lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2025-02-24 19:32:07 +0100

    refactor shapley.jl

--- src/shapley.jl
+++ src/shapley.jl
@@ -6,39 +6,27 @@ Optional argument `∅` is the payoff value for an empty coalition. If not provi
 
 Return a vector of Shapley values correspinding to each forecaster in `fs`.
 """
-function shapley(fs::Vector{<:Forecasts}, agg::Function, payoff::Function)
+function shapley(fs::Vector{<:Forecasts}, agg::Function, payoff::Function, ∅::Union{Nothing, AbstractFloat}=nothing)
     m = length(fs)
     vals = zeros(m)
+    
+    if ∅ !== nothing
+        for i in 1:m
+            vals[i] += (payoff(fs[i]) - ∅)/binomial(m-1, 0)
+        end
+    end
+    
     for s in 1:m-1
         for coalition in combinations(1:m, s)
             coalition_payoff = payoff(agg(fs[coalition]))
             for i in 1:m
                 if i ∉ coalition
-                    vals[i] += (payoff(agg(fs[vcat(coalition, i)])) - coalition_payoff)/binomial(m-1, s)
+                    vals[i] += (payoff(agg(fs[[coalition; i]])) - coalition_payoff)/binomial(m-1, s)
                 end
             end
         end
     end
-    vals /= m
-    return vals
-end
-
-function shapley(fs::Vector{<:Forecasts}, agg::Function, payoff::Function, ∅::AbstractFloat)
-    m = length(fs)
-    vals = zeros(m)
-    for i in 1:m
-        vals[i] += (payoff(fs[i]) - ∅)/binomial(m-1, 0)
-    end
-    for s in 1:m-1
-        for coalition in combinations(1:m, s)
-            coalition_payoff = payoff(agg(fs[coalition]))
-            for i in 1:m
-                if i ∉ coalition
-                    vals[i] += (payoff(agg(fs[vcat(coalition, i)])) - coalition_payoff)/binomial(m-1, s)
-                end
-            end
-        end
-    end
+    
     vals /= m
     return vals
 end
