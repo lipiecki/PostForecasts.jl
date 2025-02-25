a2c3f304678a78da13919d970fd5efe69d0c50ed
lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2025-02-25 10:55:45 +0100

    update docs

--- docs/src/examples.md
+++ docs/src/examples.md
@@ -174,16 +174,16 @@ From the decile forecasts we can construct four prediction intervals (PI) center
 ```julia
 plot(legend=:bottom, xlabel="Days", ylabel="Price (€/MWh)", xticks=1:14, framestyle=:box) 
 # plot forecasts of the median price
-plot!(viewpred(qfBUY, eachindex(qfBUY), 5), linealpha=0.5, color=3, lw=3, label="Buy price")
-plot!(viewpred(qfSELL, eachindex(qfBUY), 5), linealpha=0.5, color=1, lw=3, label="Sell price")
+plot!(viewpred(qfBUY, eachindex(qfBUY), 5), linealpha=0.5, color=theme_palette(:dark)[3], lw=3, label="Buy price")
+plot!(viewpred(qfSELL, eachindex(qfBUY), 5), linealpha=0.5, color=theme_palette(:dark)[1], lw=3, label="Sell price")
 # plot prediction intervals constructed from quantiles forecasts
 for i in 1:4
-    plot!(viewpred(qfBUY, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfBUY, eachindex(qfBUY), 5+i), fillalpha=0.1, color=3, label=nothing)
-    plot!(viewpred(qfSELL, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfSELL, eachindex(qfBUY), 5+i), fillalpha=0.1, color=1, label=nothing)    
+    plot!(viewpred(qfBUY, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfBUY, eachindex(qfBUY), 5+i), fillalpha=0.1, color=theme_palette(:dark)[3], label=nothing)
+    plot!(viewpred(qfSELL, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfSELL, eachindex(qfBUY), 5+i), fillalpha=0.1, color=theme_palette(:dark)[1], label=nothing)    
 end
 # plot observed prices
-plot!(viewobs(qfBUY), color=3, st=:scatter, markerstrokewidth=0, label=nothing)
-plot!(viewobs(qfSELL), color=1, st=:scatter, markerstrokewidth=0, label=nothing)
+plot!(viewobs(qfBUY), color=theme_palette(:dark)[3], st=:scatter, markerstrokewidth=0, label=nothing)
+plot!(viewobs(qfSELL), color=theme_palette(:dark)[1], st=:scatter, markerstrokewidth=0, label=nothing)
 ```
 ![image](images/trading.png)
 
--- examples/trading.jl
+++ examples/trading.jl
@@ -6,15 +6,15 @@ fsSELL = loaddata(Symbol(:epex, 20))    # sell at 7pm
 qfBUY = point2quant(fsBUY, method=:idr, window=182, quantiles=9, start=20230408, stop=20230421)
 qfSELL = point2quant(fsSELL, method=:idr, window=182, quantiles=9, start=20230408, stop=20230421)
 
-plot(legend=:bottom, xlabel="Days", ylabel="Price (€/MWh)", xticks=1:14, framestyle=:box)
+plot(legend=:bottom, xlabel="Days", ylabel="Price (€/MWh)", xticks=1:14, framestyle=:box) 
 # plot forecasts of the median price
-plot!(viewpred(qfBUY, eachindex(qfBUY), 5), linealpha=0.5, color=3, lw=3, label="Buy price")
-plot!(viewpred(qfSELL, eachindex(qfBUY), 5), linealpha=0.5, color=1, lw=3, label="Sell price")
+plot!(viewpred(qfBUY, eachindex(qfBUY), 5), linealpha=0.5, color=theme_palette(:dark)[3], lw=3, label="Buy price")
+plot!(viewpred(qfSELL, eachindex(qfBUY), 5), linealpha=0.5, color=theme_palette(:dark)[1], lw=3, label="Sell price")
 # plot prediction intervals constructed from quantiles forecasts
 for i in 1:4
-    plot!(viewpred(qfBUY, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfBUY, eachindex(qfBUY), 5+i), fillalpha=0.1, color=3, label=nothing)
-    plot!(viewpred(qfSELL, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfSELL, eachindex(qfBUY), 5+i), fillalpha=0.1, color=1, label=nothing)    
+    plot!(viewpred(qfBUY, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfBUY, eachindex(qfBUY), 5+i), fillalpha=0.1, color=theme_palette(:dark)[3], label=nothing)
+    plot!(viewpred(qfSELL, eachindex(qfBUY), 5-i), lw=0, fillrange=viewpred(qfSELL, eachindex(qfBUY), 5+i), fillalpha=0.1, color=theme_palette(:dark)[1], label=nothing)    
 end
 # plot observed prices
-plot!(viewobs(qfBUY), color=3, st=:scatter, markerstrokewidth=0, label=nothing)
-plot!(viewobs(qfSELL), color=1, st=:scatter, markerstrokewidth=0, label=nothing)
+plot!(viewobs(qfBUY), color=theme_palette(:dark)[3], st=:scatter, markerstrokewidth=0, label=nothing)
+plot!(viewobs(qfSELL), color=theme_palette(:dark)[1], st=:scatter, markerstrokewidth=0, label=nothing)
